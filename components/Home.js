02443c4a1ea5605be806f24cf36ce3869f776a5f
Andy Haygood <ahaygood86@gmail.com>
2019-06-29 15:34:56 -0500

    getting color selection divs draggable etc part 1

--- components/Home.js
+++ components/Home.js
@@ -20,19 +20,100 @@ const LogoBox = styled.img`
     border: 1px solid black;
 `;
 
+const Dropzone = styled.div`
+    width: 50%;
+    height: 80px;
+    border: 1px solid black;
+`;
+
 class Home extends Component {
+    
+    state = {
+        tasks: [
+          { 
+            name:"blue",
+            category:"unset",
+            color:"blue",
+            src: "../static/ia-logo-dot-blue.png"
+          },
+          { 
+            name:"red",
+            category:"unset",
+            color:"red",
+            src: "../static/ia-logo-dot-red.png"
+          },
+          { 
+            name:"green",
+            category:"unset",
+            color:"green",
+            src: "../static/ia-logo-dot-green.png"
+          },
+          { 
+            name:"black1",
+            category:"unset",
+            color:"black",
+            src: "../static/ia-logo-dot-black.png"
+          },
+          { 
+            name:"black2",
+            category:"unset",
+            color:"black",
+            src: "../static/ia-logo-dot-black.png"
+          },
+        ]
+    }
+
+    onDragOver = (e) => {
+        e.preventDefault();
+    }
+
+    onDragStart = (e, id) => {
+        console.log('onDragStart id:', id);
+        e.dataTransfer.setData("id", id);
+    }
+
+    onDrop = (e, cat) => {
+        let id = e.dataTransfer.getData("id")
+
+        let tasks = this.state.tasks.filter((task) => {
+            if (task.name == id) {
+                task.category = cat
+            }
+            return task
+        })
+
+        this.setState({
+            ...this.state,
+            tasks
+        })
+    }
+
     render() {
+
+    console.log(this.state.tasks)
+
         return (
             <HomeContent>
                 I am the home component
                 <ColorsBox>
-                    <ColorChoice src="../static/ia-logo-dot-blue.png" />
-                    <ColorChoice src="../static/ia-logo-dot-red.png" />
-                    <ColorChoice src="../static/ia-logo-dot-green.png" />
-                    <ColorChoice src="../static/ia-logo-dot-black.png" />
-                    <ColorChoice src="../static/ia-logo-dot-black.png" />
+                    {this.state.tasks.filter(task => task.category == "unset")
+                        .map(task => (
+                            <div key={task.name} 
+                                draggable
+                            >
+                                <img src={task.src}  />  
+                            </div>
+                        )
+                    )}
                 </ColorsBox>
-                <LogoBox src="../static/ia-logo-back.png" />
+                <div>
+                    <Dropzone
+                        
+                    
+                    />
+                </div>
+
+
             </HomeContent>
         );
     }
