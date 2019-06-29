886adc288a6751b464a683086472a999488c635c
Andy Haygood <ahaygood86@gmail.com>
2019-06-29 12:06:18 -0500

    styled-components boilerplate & meta info

---
+++ components/Meta.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <link href="https://fonts.googleapis.com/css?family=Kalam:400,700" rel="stylesheet" />
+    <title>InspiringApps Demo</title>
+  </Head>
+);
+
+export default Meta;
---
+++ components/Page.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
+import Meta from './Meta';
+
+const theme = {
+  backgroundColorMain: 'linear-gradient(to bottom right, #3A3A3A, #FFFFFF)',
+  backgroundColorAlt: '#FFFFFF', 
+  borderColorLogo: '#3A3A3A',
+  borderColorMain: 'grey',
+  borderColorNavMenu: 'white',
+  viewportSizePhoneMax: '360px',
+  viewportSizeTabletMin: '361px',
+  viewportSizeTabletMax: '1024px',
+  viewportSizeDesktopMin: '1025px',
+};
+
+const StyledPage = styled.div`
+  background-image: ${props => props.theme.backgroundColorMain};
+  color: ${props => props.theme.fontColorMain};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const Inner = styled.div`
+  max-width: 95vw;
+  margin: 0 auto;
+  padding: 2rem;
+  background: ${props => props.theme.backgroundColorAlt};
+  border: 2px solid ${props => props.theme.borderColorMain};
+  border-radius: 10px;
+  flex-grow: 1;
+  min-width: 80vw;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'Kalam', cursive;  
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.fontColorLink};
+  }
+`;
+
+class Page extends Component {
+
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <GlobalStyle />
+                    <Meta />
+                    <Inner>{this.props.children}</Inner>
+                </StyledPage>                
+            </ThemeProvider>
+        );
+    }
+}
+
+export default Page;
