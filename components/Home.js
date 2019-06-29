dbc863ea25946cf01a45aa82371eeef9f0665ca6
Andy Haygood <ahaygood86@gmail.com>
2019-06-29 18:20:18 -0500

    appear to have drag and drop working with appropriate color selection

--- components/Home.js
+++ components/Home.js
@@ -29,7 +29,8 @@ const Dropzone = styled.div`
 class Home extends Component {
     
     state = {
-        tasks: [
+
+        choices: [
           { 
             name:"blue",
             category:"unset",
@@ -60,57 +61,109 @@ class Home extends Component {
             color:"black",
             src: "../static/ia-logo-dot-black.png"
           },
+        ],
+
+        inputs : [
+            { 
+              name: "blue",
+              expected: "blue",
+              current: ""
+            },
+            { 
+              name: "red",
+              expected: "red",
+              current: ""
+            },
+            { 
+              name: "green",
+              expected: "green",
+              current: ""
+            },
+            { 
+              name: "black1",
+              expected: "black",
+              current: ""
+            },
+            { 
+              name: "black2",
+              expected: "black",
+              current: ""
+            }
         ]
+        
     }
 
     onDragOver = (e) => {
         e.preventDefault();
     }
 
-    onDragStart = (e, id) => {
-        console.log('onDragStart id:', id);
+    onDragStart = (e, id, color) => {
+       // console.log('onDragStart id:', id);
         e.dataTransfer.setData("id", id);
+        e.dataTransfer.setData("color", color);
+
     }
 
-    onDrop = (e, cat) => {
+    onDrop = (e, input, cat) => {
+        //id = draggble item name
+      //  console.log(e.dataTransfer.getData("id"), input)
         let id = e.dataTransfer.getData("id")
+        let color = e.dataTransfer.getData("color")
+        
+        let choices = this.state.choices.filter((choice, i) => {
+            //console.log("choice " + choice.name)
+            console.log(choice)
+            if (color == input.expected
+                && id == choice.name
+                && id != "black1" 
+                && id != "black2"
+                ) { 
+                choice.category = cat
+            } else if (color == input.expected && choice.name == id) {
+                choice.category = cat
+            }  else {
 
-        let tasks = this.state.tasks.filter((task) => {
-            if (task.name == id) {
-                task.category = cat
+                choice.category = choice.category
             }
-            return task
+            return choice
         })
+        console.log(input)
+    //    console.log(choices)
 
         this.setState({
-            ...this.state,
-            tasks
+            choices
         })
     }
-
+    
+    
     render() {
-
-    console.log(this.state.tasks)
+        
+       // console.log(this.state.inputs)
+   // console.log(this.state.choices)
 
         return (
             <HomeContent>
                 I am the home component
                 <ColorsBox>
-                    {this.state.tasks.filter(task => task.category == "unset")
-                        .map(task => (
-                            <div key={task.name} 
+                    {this.state.choices.filter(choice => choice.category == "unset")
+                        .map(choice => (
+                            <div key={choice.name}
                                 draggable
+                                onDragStart = {(e) => this.onDragStart(e, choice.name, choice.color)}
                             >
-                                <img src={task.src}  />  
+                                <img src={choice.src}  />  
                             </div>
                         )
                     )}
                 </ColorsBox>
                 <div>
-                    <Dropzone
-                        
-                    
-                    />
+                    {this.state.inputs.map(input => (
+                        <Dropzone
+                            key={input.name}
+                            onDragOver={(e)=>this.onDragOver(e)}
+                            onDrop={(e)=>this.onDrop(e, input, "complete")} 
+                        />                    
+                    ))}
                 </div>
 
 
